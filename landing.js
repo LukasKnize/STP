4cb454ed6862bdd1dfd01ab33383f47f6d137d83
LukasKnize <82168954+LukasKnize@users.noreply.github.com>
2021-12-01 14:36:16 +0100

    Add files via upload

--- landing.js
+++ landing.js
@@ -1,18 +1,23 @@
 let currentImg = 0;
 let logos = ['images/WR_Vertical_1-2048x515.png', 'images/Presloviny.cz_-1024x222.png', 'images/logo-301-simple.png'];
+let links = ['https://verticalproduction.cz/', 'https://www.presloviny.cz/', 'https://www.studio301.cz/'];
+let currentLink = 'https://verticalproduction.cz/';
 
 setInterval(() => {
     switch (currentImg) {
         case 0:
             document.getElementById("projectImg").src = logos[1];
+            currentLink = links[1];
             document.getElementById("projectImg").style.background = "transparent";
             break;
         case 1:
             document.getElementById("projectImg").src = logos[0];
+            currentLink = links[0];
             document.getElementById("projectImg").style.background = "transparent";
             break;
         case 2:
             document.getElementById("projectImg").src = logos[2];
+            currentLink = links[2];
             document.getElementById("projectImg").style.background = "#24783c";
             break;
         default:
@@ -36,3 +41,7 @@ document.getElementById("openMenu").addEventListener("click", function (){
 document.getElementById("closeMenu").addEventListener("click", function (){
     document.getElementById("menuForMobileBackground").style.display = "none";
 });
+
+document.getElementById("projectImg").addEventListener('click', () => {
+    window.open(currentLink);
+})
